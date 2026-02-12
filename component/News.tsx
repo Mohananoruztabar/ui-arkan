ef4c93d97d4384054f97ff0fc1712d27e342cca3
mohana nr <@gmail.com>
2026-02-12 09:27:13 -0800

    news

---
+++ component/News.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Container from './Container'
+import { FaStar } from "react-icons/fa";
+import { PiClockFill } from "react-icons/pi";
+
+function News() {
+  return (
+    <div className='mt-16'>
+        <Container>
+            <div className='flex justify-between text-right'>
+                <h3 className='text-gray-600 text-lg ml-2 lg:ml-0 lg:text-2xl font-bold'> مشاهده همه +</h3>
+                <h2 className='text-2xl text-gray-600 font-bold'>آخرین اخبار و مطالب سایت</h2>
+            </div>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-5 mt-10'>
+                <div className='bg-white shadow1 rounded-xl p-2'>
+                    <figure>
+                        <img src="/photo-1589361580298-73eaccc5fd96.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm font-bold text-gray-700 mt-3'>لورم ایپسوم متن ساختگی تیم طراحی سپهر پلاس برای قسمت عنوان وبلاگ</h4>
+                    <span className='block border-dashed border border-gray-400 w-full mt-3'></span>
+                    <p className='text-gray-500 text-[12px] lg:text-sm text-right mt-3'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای زیبا سازی مطالب</p>
+                    <div className='flex justify-between mt-3'>
+                        <ul className='flex items-center flex-row-reverse'>
+                            <li className='text-gray-500 ml-2 text-lg lg:text-xl'><PiClockFill /></li>
+                            <li className='text-gray-500 text-sm'>یک دقیقه </li>
+                        </ul>
+                        <div className='flex justify-end-safe items-center'>
+                            <h2 className='text-gray-600 text-sm'>صمد سپهری</h2>
+                            <figure className='-mr-3'>
+                                <img src="/images.png" alt="" />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-white shadow1 rounded-xl p-2'>
+                    <figure>
+                        <img src="/photo-1548611716-3000815a5803.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm font-bold text-gray-700 mt-3'>لورم ایپسوم متن ساختگی تیم طراحی سپهر پلاس برای قسمت عنوان وبلاگ</h4>
+                    <span className='block border-dashed border border-gray-400 w-full mt-3'></span>
+                    <p className='text-gray-500 text-[12px] lg:text-sm text-right mt-3'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای زیبا سازی مطالب</p>
+                    <div className='flex justify-between mt-3'>
+                        <ul className='flex items-center flex-row-reverse'>
+                            <li className='text-gray-500 ml-2 text-lg lg:text-xl'><PiClockFill /></li>
+                            <li className='text-gray-500 text-sm'>یک دقیقه </li>
+                        </ul>
+                        <div className='flex justify-end-safe items-center'>
+                            <h2 className='text-gray-600 text-sm'>صمد سپهری</h2>
+                            <figure className='-mr-3'>
+                                <img src="/images.png" alt="" />
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div className='bg-white shadow1 rounded-xl p-2'>
+                     <figure>
+                        <img src="/photo-1590650516494-0c8e4a4dd67e.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm font-bold text-gray-700 mt-3'>لورم ایپسوم متن ساختگی تیم طراحی سپهر پلاس برای قسمت عنوان وبلاگ</h4>
+                    <span className='block border-dashed border border-gray-400 w-full mt-3'></span>
+                    <p className='text-gray-500 text-right text-[12px] lg:text-sm mt-3'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است برای زیبا سازی مطالب</p>
+                    <div className='flex justify-between mt-3'>
+                        <ul className='flex items-center flex-row-reverse'>
+                            <li className='text-gray-500 ml-2 text-lg lg:text-xl'><PiClockFill /></li>
+                            <li className='text-gray-500 text-sm'>یک دقیقه </li>
+                        </ul>
+                        <div className='flex justify-end-safe items-center'>
+                            <h2 className=' text-gray-600 text-sm'>صمد سپهری</h2>
+                            <figure className='-mr-3'>
+                                <img src="/images.png" alt="" />
+                            </figure>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default News
