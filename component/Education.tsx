6922ce8502a03556dd932c37ed756b6ebebc9237
mohana nr <@gmail.com>
2026-02-12 09:10:45 -0800

    Eduction component

---
+++ component/Education.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import Container from './Container'
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
+
+function Education() {
+  return (
+    <div className='mt-16'>
+        <Container>
+            <div className='flex justify-between text-right'>
+                <h3 className='text-gray-600 text-lg ml-2 lg:ml-0 lg:text-2xl font-bold'> مشاهده همه +</h3>
+                <h2 className='text-2xl text-gray-600 font-bold'>جدید ترین آموزش های ما</h2>
+            </div>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-5 mt-10'>
+                <div className='bg-white shadow1 rounded-xl p-2'>
+                    <figure>
+                        <img src="/Communication-skills-400x251.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm lg:text-xl text-gray-600 mt-4'>اسم آموزش پیش فرض شما</h4>
+                    <div className='flex justify-between items-center'>
+                        <ul className='flex'>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                        </ul>
+                        <div className='flex lg:justify-end-safe items-center mt-2'>
+                            <h2 className='text-sm text-gray-600'>صمد سپهری</h2>
+                            <figure className='-mr-4'>
+                                <img className='lg:w-20 w-14' src="/foto-SORRISO-LINEAR.png" alt="" />
+                            </figure>
+                        </div>
+                    </div>
+                    <span className='block border-dashed border border-gray-400 w-full'></span>
+                    <div className='text-right flex justify-between mt-5 items-center'>
+                        <button className='w-36 h-10 lg:w-44 lg:h-12 text-[12px] lg:text-sm bg-btn2 text-gray-600 ml-5 rounded-lg font-bold'>ثبت نام در دوره</button>
+                        <h5 className='flex flex-row-reverse text-money font-bold text-[12px] lg:text-sm'><span className='ml-2 text-[12px] lg:text-sm text-money font-light'>67,000</span>تومان</h5>
+                    </div>
+                </div>
+                <div className='bg-white shadow1 rounded-xl p-2 mt-2'>
+                    <figure>
+                        <img src="/marketing-and-sales-in-business-400x251.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm lg:text-xl text-gray-600 mt-4'>اسم آموزش پیش فرض شما</h4>
+                    <div className='flex justify-between items-center'>
+                        <ul className='flex'>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                        </ul>
+                        <div className='flex lg:justify-end-safe items-center mt-2'>
+                            <h2 className='text-sm text-gray-600'>صمد سپهری</h2>
+                            <figure className='-mr-4'>
+                                <img className='lg:w-20 w-14' src="/foto-SORRISO-LINEAR.png" alt="" />
+                            </figure>
+                        </div>
+                    </div>
+                    <span className='block border-dashed border border-gray-400 w-full'></span>
+                    <div className='text-right flex justify-between mt-5 items-center'>
+                        <button className='w-36 h-10 lg:w-44 lg:h-12 text-[12px] lg:text-sm bg-btn2 text-gray-600 ml-5 rounded-lg font-bold'>ثبت نام در دوره</button>
+                        <h5 className='flex flex-row-reverse text-money font-bold text-[12px] lg:text-sm'>رایگان</h5>
+                    </div>
+                </div>
+                <div className='bg-white shadow1 rounded-xl p-2'>
+                    <figure>
+                        <img src="/Methods-of-increasing-productivity-400x251.png" alt="" />
+                    </figure>
+                    <h4 className='text-right text-sm lg:text-xl text-gray-600 mt-4'>اسم آموزش پیش فرض شما</h4>
+                    <div className='flex justify-between items-center'>
+                        <ul className='flex'>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg1 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                            <li className='text-starbg2 lg:text-2xl'><FaStar /></li>
+                        </ul>
+                        <div className='flex lg:justify-end-safe items-center mt-2'>
+                            <h2 className='text-sm text-[12px] text-gray-600'>صمد سپهری</h2>
+                            <figure className='-mr-4'>
+                                <img className='lg:w-20 w-14' src="/foto-SORRISO-LINEAR.png" alt="" />
+                            </figure>
+                        </div>
+                    </div>
+                    <span className='block border-dashed border border-gray-400 w-full'></span>
+                    <div className='text-right flex justify-between mt-5 items-center'>
+                        <button className='w-36 h-10 lg:w-44 lg:h-12 text-[12px] lg:text-sm bg-btn2  text-gray-600 ml-5 rounded-lg font-bold'>ثبت نام در دوره</button>
+                        <h5 className='flex flex-row-reverse text-[12px] lg:text-sm text-money font-bold'><span className='ml-2 text-[12px] lg:text-sm text-money font-light'>67,000</span>تومان</h5>
+                    </div>
+                </div>
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default Education
