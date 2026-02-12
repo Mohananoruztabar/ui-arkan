b7f04d3bf1e39d4cce5e4ae826866ea251e21ab3
mohana nr <@gmail.com>
2026-02-12 09:18:37 -0800

    layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/component/Navbar";
+import Footer from "@/component/Footer";
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const familyFont = localFont({
+  src: [
+    {
+      path: '../public/font/Yekan-Bakh-Family/ttf/YekanBakh-Regular.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/yekan-bakh-fanum/ttf/YekanBakhFaNum-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/font/Yekan-Bakh-NoEn/ttf/YekanBakhNoEn-Regular.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-family',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +37,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="fa" className={familyFont.variable}>
+      <body>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
